f8e3d75a1ca7e18d1140653cc5873a30d3af7ade
simonescaccia <72872543+simonescaccia@users.noreply.github.com>
2021-12-06 16:40:28 +0100

    v1.0

--- Models/LifeCalendar.py
+++ Models/LifeCalendar.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 from PIL import Image, ImageDraw, ImageFont
 import calendar
 from Models.Birthdate import Birthdate
@@ -19,12 +19,17 @@ class LifeCalendar:
     pathnameFont = "Fonts\CALIST.TTF"
 
     #misures in pixels
+    numberTextPixelsStartingError = 7
+
     titleFontSize = 100
-    numberFontSize = 35
-    numberWidth = 45
+    numberFontSize = 34
+    oneDigitNumberWidth = 27
+    twoDigitNumberWidth = 45
+
     startTextPosition = (750,50)
     startSquaresPositionX = 250
     startSquaresPositionY = 200
+
     squaresDimension = 25
     squaresMargin = 10
     spaceBetweenMonthsYears = 10
@@ -39,13 +44,13 @@ class LifeCalendar:
 
     #create My life in weeks calendar
     def fillCalendar(self):
-        #add text
+        #add title on Calendar
         self.addTitle()
 
-        #add squares
+        #add squares on Calendar
         self.addSquares()
 
-        #add numbers
+        #add numbers on Calendar
         self.addNumbers()
 
     #add title to image
@@ -65,12 +70,12 @@ class LifeCalendar:
             for x in range(52):
             
                 #space between months
-                if((x != 0) & (x % 4 == 0)):
+                if ((x != 0) & (x % 4 == 0)):
                     endMonthFlag += 1
 
                 #space between years
                 #(y/10 != endTenYearFlag) because it needs to increment only one time when (y % 10 == 0)
-                if((y != 0) & (y % 10 == 0) & (y/10 != endTenYearFlag)):
+                if ((y != 0) & (y % 10 == 0) & (y/10 != endTenYearFlag)):
                     endTenYearFlag += 1
 
                 positionX = self.startSquaresPositionX + x * (self.squaresMargin + self.squaresDimension) + endMonthFlag * self.spaceBetweenMonthsYears 
@@ -78,7 +83,7 @@ class LifeCalendar:
 
                 #choose black or white square
                 isBlack = self.isBlackSquare(x, y)
-                if(isBlack):
+                if (isBlack):
                     color = 'black'
                 else: 
                     color = 'white'
@@ -91,25 +96,27 @@ class LifeCalendar:
 
         self.img.save(self.pathnameImage)
 
+    # week = range [0, 51]
+    # year = range [0, 89]
     def isBlackSquare(self, week, year):
-        todayDate = date.today()
-        todayYear = todayDate.year
-        if(self.birthdate.date.year + year + 1 < todayYear):
-            return True
-        elif(self.birthdate.date.year + year + 1 == todayYear):
-            #calculate the weeks differece between today and birthday
-            birthdate = date(
-                self.birthdate.date.year + year + 1, 
-                self.birthdate.date.month, 
-                self.birthdate.date.day)
-            delta = todayDate - birthdate
-            weeks = int(delta.days/7)
-            if(weeks - week > 0):
-                return True
-            else:
-                return False
-        else:
-            return False
+
+        todayDate = datetime.today()
+        datePointed = self.birthdate.date
+
+        """Return a date that's `year` years after the date (or datetime)
+        object `datePointed`. Return the same calendar date (month and day) in the
+        destination year, if it exists, otherwise use the following day
+        (thus changing February 29 to March 1).
+        """
+        try:
+            datePointed = datePointed.replace(year = datePointed.year + year)
+        except ValueError:
+            datePointed = datePointed + (date(datePointed.year + year, 1, 1) - date(datePointed.year, 1, 1))
+
+        datePointed = datePointed + timedelta(weeks = week + 1)
+
+        return datePointed < todayDate
+
 
     #draw the image
     def drawSquare(self, positionX, positionY, color):
@@ -133,21 +140,27 @@ class LifeCalendar:
             width=2)
 
     def addNumbers(self):
-        for y in range(self.years + 1):
-
-            endTenYearFlag = 0
+        endTenYearFlag = 0
+        for y in range(self.years):
             #space between years
             #(y/10 != endTenYearFlag) because it needs to increment only one time when (y % 10 == 0)
-            if((y != 0) & (y % 10 == 0) & (y/10 != endTenYearFlag)):
+            if ((y != 0) & (y % 10 == 0) & (y/10 != endTenYearFlag)):
                 endTenYearFlag += 1
-            positionY = self.startSquaresPositionY + y * (self.squaresMargin + self.squaresDimension) + endTenYearFlag * self.spaceBetweenMonthsYears
 
-            if(y%5 == 0):
+            positionY = self.startSquaresPositionY + y * (self.squaresMargin + self.squaresDimension) + endTenYearFlag * self.spaceBetweenMonthsYears - self.numberTextPixelsStartingError
+
+            #right alignment for numbers with one digit
+            if (y < 10):
+                positionX = self.startSquaresPositionX - self.oneDigitNumberWidth
+            else:
+                positionX = self.startSquaresPositionX - self.twoDigitNumberWidth
+            
+            if (y%5 == 0):
                 d = ImageDraw.Draw(self.img)
                 fnt = ImageFont.truetype(self.pathnameFont, self.numberFontSize)
                 d.text((
-                    self.startSquaresPositionX - self.numberWidth,
+                    positionX,
                     positionY
                     ), str(y), font=fnt, fill=(0, 0, 0))
         
-                self.img.save(self.pathnameImage)
+        self.img.save(self.pathnameImage)
